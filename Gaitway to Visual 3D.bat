baca2cb500c52698c9feac958a69a3ad5010faed
Jejmule <jejmule@gmail.com>
2024-12-02 13:13:46 +0100

    debug

---
+++ Gaitway to Visual 3D.bat
@@ -0,0 +1,4 @@
+echo off
+cls
+"%windir%\System32\cmd.exe" /k ""C:\Users\User\miniconda3\Scripts\activate.bat" "C:\Users\User\miniconda3" && python "C:\Users\User\Documents\ISSUL-Gaitway2Visual3D-main\gaitway2visual3D.py " && exit"
+
